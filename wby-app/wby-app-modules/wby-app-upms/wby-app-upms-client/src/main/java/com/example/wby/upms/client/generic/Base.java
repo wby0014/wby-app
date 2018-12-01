5738a96701dcd5aa2c9df331882580945f589050
wby0014 <106427479@qq.com>
2018-12-01 12:10:13 +0800

    提交更改

--- wby-app/wby-app-modules/wby-app-upms/wby-app-upms-client/src/main/java/com/example/wby/upms/client/generic/Base.java
+++ wby-app/wby-app-modules/wby-app-upms/wby-app-upms-client/src/main/java/com/example/wby/upms/client/generic/Base.java
@@ -1,4 +1,23 @@
 package com.example.wby.upms.client.generic;
 
-public class Base {
+public class Base implements Comparable<Base> {
+
+    private int sortOrder;
+
+    public Base(int sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    @Override
+    public int compareTo(Base o) {
+        if (sortOrder < o.sortOrder) {
+            return -1;
+        } else if (sortOrder > o.sortOrder) {
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+
 }
--- wby-app/wby-app-modules/wby-app-upms/wby-app-upms-client/src/main/java/com/example/wby/upms/client/generic/Child.java
+++ wby-app/wby-app-modules/wby-app-upms/wby-app-upms-client/src/main/java/com/example/wby/upms/client/generic/Child.java
@@ -1,4 +1,8 @@
 package com.example.wby.upms.client.generic;
 
-public class Child {
+public class Child extends Base {
+
+    public Child(int sortOrder) {
+        super(sortOrder);
+    }
 }
